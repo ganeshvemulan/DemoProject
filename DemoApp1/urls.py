ed7c454c99197c835ac19d61e4ef1f037fff1204
ganeshvemulan <ganeshvemulan@gmail.com>
2023-07-13 00:45:28 +0530

    Initial commit

---
+++ DemoApp1/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path;	#new
+#from django.urls import re_path;
+from DemoApp1 import views;
+
+urlpatterns = [ 
+	path('first/', views.f1), 
+	path('second/', views.f2),
+];
---
+++ DemoApp2/urls.py
@@ -0,0 +1,10 @@
+#from django.conf.urls import url;	#old
+from django.urls import path;	#new
+#from django.urls import re_path;
+
+from DemoApp2 import views;
+
+urlpatterns = [ 
+	path('third/', views.f3), 
+	path('fourth/', views.f4),
+];
